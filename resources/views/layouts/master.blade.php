6a543c9583004767e90e94fdb735c2d130663e92
eMedin4 <elannmedina@gmail.com>
2016-10-25 10:38:47 +0200

    v0.212

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,18 +7,16 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="csrf-token" content="{{ csrf_token() }}">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:700|Open+Sans:400,600" rel="stylesheet">
-	<script src="https://use.fortawesome.com/96155e14.js"></script>
 	<link rel="stylesheet" href="{{ asset('/assets/css/style.css') }}">
 
 	<!-- favicons -->
-		<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('/assets/favicon/apple-touch-icon.png') }}">
+<!-- 		<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('/assets/favicon/apple-touch-icon.png') }}">
 		<link rel="icon" type="image/png" href="{{ asset('/assets/favicon/favicon-32x32.png') }}" sizes="32x32">
 		<link rel="icon" type="image/png" href="{{ asset('/assets/favicon/favicon-16x16.png') }}" sizes="16x16">
 		<link rel="manifest" href="{{ asset('/assets/favicon/manifest.json') }}">
 		<link rel="shortcut icon" href="{{ asset('/assets/favicon/favicon.ico') }}">
 		<meta name="msapplication-config" content="{{ asset('/assets/favicon/browserconfig.xml') }}">
-		<meta name="theme-color" content="#ffffff">
+		<meta name="theme-color" content="#ffffff"> -->
 	<!-- end favicons -->
 
 	<script>
@@ -44,11 +42,11 @@
 			<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 			<script src="{{ asset('/assets/js/scripts.js') }}"></script>
 
-		<!--Page scripts-->
-			@yield('scripts')
-
 	</div>
 
+<!--Page scripts-->yield('scripts')
+<link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:700|Open+Sans:400,600" rel="stylesheet">
+<script src="https://use.fortawesome.com/96155e14.js"></script>
 </body>
 </html>
 
